e4412dc615b74e0f671745333521dd29b214dd3f
vadimbvn <Sky4nii@yandex.com>
2020-07-02 02:07:29 +0300

    v.6

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,15 @@
-var mainNav = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".main-nav__toggle");
+'use strict';
+var mainNav = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-mainNav.classList.remove("main-nav--nojs");
+mainNav.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener("click", function() {
-  if (mainNav.classList.contains("main-nav--closed")) {
-    mainNav.classList.remove("main-nav--closed");
-    mainNav.classList.add("main-nav--open");
+navToggle.addEventListener('click', function() {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--open');
   } else {
-    mainNav.classList.add("main-nav--closed");
-    mainNav.classList.remove("main-nav--open");
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--open');
   }
 });
